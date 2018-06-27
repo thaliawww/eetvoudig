178af354821664263df3532524bb76abf75855fe
Jelle Besseling <jelle@pingiun.com>
2018-06-27 21:14:21 +0200

    Do migrations before chown

--- resources/entrypoint_production.sh
+++ resources/entrypoint_production.sh
@@ -2,13 +2,13 @@
 
 set -e
 
+python /usr/src/app/manage.py migrate --no-input
+python /usr/src/app/manage.py collectstatic --no-input
+
 chown -R www-data:www-data /eetvoudig/
 chown -R www-data:www-data /usr/src/app
 
 cd /usr/src/app/
-
-python /usr/src/app/manage.py migrate --no-input
-python /usr/src/app/manage.py collectstatic --no-input
 
 >&2 echo "Running site with uwsgi"
 uwsgi --chdir /usr/src/app \
