1e6d1bb001bf2ef890be39a9c07be764deb8f67e
Joren Vrancken <jorenvrancken@gmail.com>
2019-01-15 11:48:56 +0100

    Remove unused SecurityMiddleware

--- eetvoudig/settings.py
+++ eetvoudig/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'eetvoudig.urls'
