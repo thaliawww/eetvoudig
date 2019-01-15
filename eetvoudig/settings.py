08d2944552037d7546273465860acd35927bbae7
Joren Vrancken <jorenvrancken@gmail.com>
2019-01-15 11:30:07 +0100

    Remove XFrameOptionsMiddleware

--- eetvoudig/settings.py
+++ eetvoudig/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 ]
 
